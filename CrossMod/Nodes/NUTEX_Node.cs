3676775ebbb34fb464dc73a6d53b3881244ea1c0
ploaj_the_otter <ploaj_the_otter@yahoo.com>
2018-12-02 20:20:45 -0600

    fixed swizzling

--- CrossMod/Nodes/NUTEX_Node.cs
+++ CrossMod/Nodes/NUTEX_Node.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using OpenTK.Graphics.OpenGL;
 using System.IO;
 using CrossMod.Tools;
+using OpenTK;
 
 namespace CrossMod.Nodes
 {
@@ -82,7 +83,7 @@ namespace CrossMod.Nodes
                     MipSizes[i] = R.ReadInt32();
                 R.ReadChars(4); // TNX magic
                 TexName = "";
-                for(int i = 0; i < 64; i++)
+                for(int i = 0; i < 0x40; i++)
                 {
                     byte b = R.ReadByte();
                     if (b != 0)
@@ -93,20 +94,109 @@ namespace CrossMod.Nodes
                 int Unk = R.ReadInt32();
                 Format = (NUTEX_FORMAT)R.ReadByte();
                 R.ReadByte();
-                R.ReadInt16();
-                int Alignment = R.ReadInt32();
+                ushort Padding = R.ReadUInt16();
+                R.ReadUInt32();
                 int MipCount = R.ReadInt32();
-                int Array = R.ReadInt32();
+                int Alignment = R.ReadInt32();
+                int ArrayCount = R.ReadInt32();
                 int ImageSize = R.ReadInt32();
                 char[] Magic = R.ReadChars(4);
                 int MajorVersion = R.ReadInt16();
                 int MinorVersion = R.ReadInt16();
 
+
+                if (Format != NUTEX_FORMAT.BC7_UNORM && Format != NUTEX_FORMAT.BC7_SRGB)
+                    return;
+
+                uint blkWidth = (uint)blkdims[Format].X;
+                uint blkHeight = (uint)blkdims[Format].Y;
+
+                uint blockHeight = SwitchSwizzler.GetBlockHeight(SwitchSwizzler.DIV_ROUND_UP((uint)Height, blkHeight));
+                uint BlockHeightLog2 = (uint)Convert.ToString(blockHeight, 2).Length - 1;
+                uint tileMode = 0;
+
+                //int linesPerBlockHeight = (1 << (int)BlockHeightLog2) * 8;
+
+                uint bpp = bpps(Format);
+
                 R.BaseStream.Position = 0;
-                for (int i = 0; i < 1; i++) // TODO: fix mip alignment v=MipCount
+                int blockHeightShift = 0;
+                for (int i = 0; i < 1; i++)
                 {
-                    Mipmaps.Add(SwitchSwizzler.Deswizzle(Width >> i, Height >> i, 0x1E, R.ReadBytes(MipSizes[i])));
+                    int size = MipSizes[i];
+
+                    if (i == 0)
+                        if (size % Alignment != 0)
+                            size = size + (Alignment - (size % Alignment));
+
+                    //if (SwitchSwizzler.pow2_round_up(SwitchSwizzler.DIV_ROUND_UP((uint)Height, blkWidth)) < linesPerBlockHeight)
+                    //    blockHeightShift += 1;
+                    //Console.WriteLine($"{Path} {Width} {Height} {bpp} {Format} {size} {MipSizes[0]} {blkWidth} {blkHeight}");
+                    try
+                    {
+                        //Mipmaps.Add(SwitchSwizzler.deswizzle((uint)(Width >> i), (uint)(Height >> i), blkWidth, blkHeight, 1, bpp, tileMode, (int)Math.Max(0, BlockHeightLog2 - blockHeightShift), R.ReadBytes(MipSizes[i])));
+                        byte[] deswiz = SwitchSwizzler.deswizzle((uint)Width, (uint)Height, blkWidth, blkHeight, 0, bpp, tileMode, (int)Math.Max(0, BlockHeightLog2 - blockHeightShift), R.ReadBytes(ImageSize));
+                        byte[] trimmed = new byte[MipSizes[0]];
+                        Array.Copy(deswiz, 0, trimmed, 0, trimmed.Length);
+
+                        Mipmaps.Add(trimmed);
+                    }
+                    catch
+                    {
+
+                    }
                 }
+            }
+        }
+
+        public static Dictionary<NUTEX_FORMAT, Vector2> blkdims = new Dictionary<NUTEX_FORMAT, Vector2>()
+        {
+            { NUTEX_FORMAT.B8G8R8A8_UNORM, new Vector2(1, 1) },
+            { NUTEX_FORMAT.B8G8R8A8_SRGB, new Vector2(1, 1) },
+            { NUTEX_FORMAT.R8G8B8A8_UNORM, new Vector2(1, 1) },
+            { NUTEX_FORMAT.R8G8B8A8_SRGB, new Vector2(1, 1) },
+            { NUTEX_FORMAT.R32G32B32A32_FLOAT, new Vector2(1, 1) },
+            { NUTEX_FORMAT.BC1_UNORM, new Vector2(1, 1) },
+            { NUTEX_FORMAT.BC1_SRGB, new Vector2(1, 1) },
+            { NUTEX_FORMAT.BC2_UNORM, new Vector2(1, 1) },
+            { NUTEX_FORMAT.BC2_SRGB, new Vector2(1, 1) },
+            { NUTEX_FORMAT.BC3_UNORM, new Vector2(1, 1) },
+            { NUTEX_FORMAT.BC3_SRGB, new Vector2(1, 1) },
+            { NUTEX_FORMAT.BC4_UNORM, new Vector2(1, 1) },
+            { NUTEX_FORMAT.BC4_SNORM, new Vector2(1, 1) },
+            { NUTEX_FORMAT.BC5_UNORM, new Vector2(1, 1) },
+            { NUTEX_FORMAT.BC5_SNORM, new Vector2(1, 1) },
+            { NUTEX_FORMAT.BC6_UFLOAT, new Vector2(1, 1) },
+            { NUTEX_FORMAT.BC7_SRGB, new Vector2(4, 4) },
+            { NUTEX_FORMAT.BC7_UNORM, new Vector2(4, 4) },
+        };
+
+        public static uint bpps(NUTEX_FORMAT format)
+        {
+            switch (format)
+            {
+                case NUTEX_FORMAT.R8G8B8A8_UNORM:
+                case NUTEX_FORMAT.R8G8B8A8_SRGB:
+                    return 4;
+
+                case NUTEX_FORMAT.BC1_UNORM:
+                case NUTEX_FORMAT.BC1_SRGB:
+                case NUTEX_FORMAT.BC4_UNORM:
+                case NUTEX_FORMAT.BC4_SNORM:
+                    return 8;
+
+                case NUTEX_FORMAT.R32G32B32A32_FLOAT:
+                case NUTEX_FORMAT.BC2_UNORM:
+                case NUTEX_FORMAT.BC2_SRGB:
+                case NUTEX_FORMAT.BC3_UNORM:
+                case NUTEX_FORMAT.BC3_SRGB:
+                case NUTEX_FORMAT.BC5_UNORM:
+                case NUTEX_FORMAT.BC5_SNORM:
+                case NUTEX_FORMAT.BC6_UFLOAT:
+                case NUTEX_FORMAT.BC7_UNORM:
+                case NUTEX_FORMAT.BC7_SRGB:
+                    return 16;
+                default: return 0x00;
             }
         }
 
