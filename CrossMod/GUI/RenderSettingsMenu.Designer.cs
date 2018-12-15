4645833a7c37f8eba5135622705ca233b4493789
SMG <scandanavianmountaingoat@gmail.com>
2018-12-15 14:42:20 -0600

    resize render settings menu

--- CrossMod/GUI/RenderSettingsMenu.Designer.cs
+++ CrossMod/GUI/RenderSettingsMenu.Designer.cs
@@ -37,14 +37,14 @@
             this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(327, 610);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(431, 768);
             this.flowLayoutPanel1.TabIndex = 0;
             // 
             // RenderSettingsMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(327, 610);
+            this.ClientSize = new System.Drawing.Size(431, 768);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Name = "RenderSettingsMenu";
             this.Text = "Render Settings";
