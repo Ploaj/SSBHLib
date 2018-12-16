0bdf51c66f43f7ab37dd839fee26eafbf272b3b2
SMG <scandanavianmountaingoat@gmail.com>
2018-12-15 19:15:15 -0600

    move generic material creation to material class

--- CrossMod/Rendering/Material.cs
+++ CrossMod/Rendering/Material.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections.Generic;
 using SFGraphics.GLObjects.Textures;
+using SFGenericModel.Materials;
+using OpenTK;
 
 namespace CrossMod.Rendering
 {
     public class Material
     {
+        private Resources.DefaultTextures defaultTextures;
         public Texture col = null;
 
         public Texture col2 = null;
@@ -20,12 +23,15 @@ namespace CrossMod.Rendering
 
         public Texture gao = null;
 
-        public Dictionary<long, OpenTK.Vector4> vec4ByParamId = new Dictionary<long, OpenTK.Vector4>();
+        public Dictionary<long, Vector4> vec4ByParamId = new Dictionary<long, Vector4>();
         public Dictionary<long, bool> boolByParamId = new Dictionary<long, bool>();
         public Dictionary<long, float> floatByParamId = new Dictionary<long, float>();
 
         public Material(Resources.DefaultTextures defaultTextures)
         {
+            // TODO: Don't store another reference.
+            this.defaultTextures = defaultTextures;
+
             // Ensure the textures are never null, so we can modify their state later.
             col = defaultTextures.defaultBlack;
             col2 = defaultTextures.defaultBlack;
@@ -34,6 +40,134 @@ namespace CrossMod.Rendering
             emi = defaultTextures.defaultBlack;
             bakeLit = defaultTextures.defaultBlack;
             gao = defaultTextures.defaultWhite;
+        }
+
+        public GenericMaterial CreateGenericMaterial(Material material)
+        {
+            // Don't use the default texture unit.
+            var genericMaterial = new GenericMaterial(1);
+
+            AddTextures(genericMaterial);
+            AddMaterialParams(genericMaterial);
+
+            return genericMaterial;
+        }
+
+        private void AddTextures(GenericMaterial genericMaterial)
+        {
+            AddMaterialTextures(genericMaterial);
+
+            AddImageBasedLightingTextures(genericMaterial);
+
+            AddRenderModeTextures(genericMaterial);
+        }
+
+        private void AddMaterialParams(GenericMaterial genericMaterial)
+        {
+            // Set specific parameters and use a default value if not present.
+            AddVec4("vec4Param", genericMaterial, RenderSettings.Instance.ParamId, new Vector4(0));
+
+            // Assume no edge lighting if not present.
+            AddVec4("paramA6", genericMaterial, 0xA6, new Vector4(0));
+
+            // Some sort of skin subsurface color?
+            AddVec4("paramA3", genericMaterial, 0xA3, new Vector4(1));
+
+            // Sprite sheet UV parameters.
+            AddVec4("paramAA", genericMaterial, 0xAA, new Vector4(1));
+
+            // Enables/disables specular occlusion.
+            AddBool("paramE9", genericMaterial, 0xE9, true);
+
+            // Controls anisotropic specular.
+            AddFloat("paramCA", genericMaterial, 0xCA, 0.0f);
+
+            // Alpha offset.
+            AddVec4("param98", genericMaterial, 0x98, new Vector4(1, 0, 0, 0));
+        }
+
+        private void AddMaterialTextures(GenericMaterial genericMaterial)
+        {
+            // Use black for the default value.
+            // Some materials seem to use emission as the main diffuse.
+            genericMaterial.AddTexture("colMap", col);
+
+            // Use the first texture for both layers if the second layer isn't present.
+            if (HasCol2)
+                genericMaterial.AddTexture("col2Map", col2);
+            else
+                genericMaterial.AddTexture("col2Map", col);
+
+            genericMaterial.AddTexture("prmMap", prm);
+            genericMaterial.AddTexture("norMap", nor);
+            genericMaterial.AddTexture("emiMap", emi);
+            genericMaterial.AddTexture("bakeLitMap", bakeLit);
+            genericMaterial.AddTexture("gaoMap", gao);
+        }
+
+        private void AddRenderModeTextures(GenericMaterial genericMaterial)
+        {
+            genericMaterial.AddTexture("uvPattern", defaultTextures.uvPattern);
+        }
+
+        private void AddImageBasedLightingTextures(GenericMaterial genericMaterial)
+        {
+            genericMaterial.AddTexture("diffusePbrCube", defaultTextures.diffusePbr);
+            genericMaterial.AddTexture("specularPbrCube", defaultTextures.specularPbr);
+            genericMaterial.AddTexture("iblLut", defaultTextures.iblLut);
+        }
+
+        private void AddBool(string name, GenericMaterial genericMaterial, long paramId, bool defaultValue)
+        {
+            if (boolByParamId.ContainsKey(paramId))
+            {
+                var value = boolByParamId[paramId];
+                genericMaterial.AddBoolToInt(name, value);
+            }
+            else
+            {
+                genericMaterial.AddBoolToInt(name, defaultValue);
+            }
+        }
+
+        private void AddFloat(string name, GenericMaterial genericMaterial, long paramId, float defaultValue)
+        {
+            if (floatByParamId.ContainsKey(paramId))
+            {
+                var value = floatByParamId[paramId];
+                genericMaterial.AddFloat(name, value);
+            }
+            else
+            {
+                genericMaterial.AddFloat(name, defaultValue);
+            }
+        }
+
+        private void AddVec4(string name, GenericMaterial genericMaterial, long paramId, Vector4 defaultValue)
+        {
+            // Convert parameters into colors for easier visualization.
+            if (vec4ByParamId.ContainsKey(paramId))
+            {
+                var value = vec4ByParamId[paramId];
+                genericMaterial.AddVector4(name, value);
+            }
+            else if (boolByParamId.ContainsKey(paramId))
+            {
+                var value = boolByParamId[paramId];
+                if (value)
+                    genericMaterial.AddVector4(name, new Vector4(1, 0, 1, 0));
+                else
+                    genericMaterial.AddVector4(name, new Vector4(0, 0, 1, 0));
+            }
+            else if (floatByParamId.ContainsKey(paramId))
+            {
+                var value = floatByParamId[paramId];
+                genericMaterial.AddVector4(name, new Vector4(value, value, value, 0));
+            }
+            else
+            {
+                genericMaterial.AddVector4(name, defaultValue);
+            }
         }
     }
 }
--- CrossMod/Rendering/Models/RMesh.cs
+++ CrossMod/Rendering/Models/RMesh.cs
@@ -1,10 +1,7 @@
-﻿using OpenTK.Graphics.OpenGL;
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
 using SFGraphics.Cameras;
 using SFGraphics.GLObjects.Shaders;
-using System.Collections.Generic;
-using OpenTK;
-using SFGenericModel.Materials;
-using SFGraphics.GLObjects.Textures;
 
 namespace CrossMod.Rendering.Models
 {
@@ -50,144 +47,12 @@ namespace CrossMod.Rendering.Models
 
         private void SetTextureUniforms(Shader shader)
         {
+            // TODO: Rework this.
             if (defaultTextures == null)
                 defaultTextures = new Resources.DefaultTextures();
 
-            var genericMaterial = CreateGenericMaterial(Material);
-
+            var genericMaterial = Material.CreateGenericMaterial(Material);
             genericMaterial.SetShaderUniforms(shader);
-        }
-
-        private GenericMaterial CreateGenericMaterial(Material material)
-        {
-            // Don't use the default texture unit.
-            var genericMaterial = new GenericMaterial(1);
-
-            AddTextures(material, genericMaterial);
-            AddMaterialParams(material, genericMaterial);
-
-            return genericMaterial;
-        }
-
-        private static void AddTextures(Material material, GenericMaterial genericMaterial)
-        {
-            AddMaterialTextures(material, genericMaterial);
-
-            AddImageBasedLightingTextures(genericMaterial);
-
-            AddRenderModeTextures(genericMaterial);
-        }
-
-        private static void AddMaterialParams(Material material, GenericMaterial genericMaterial)
-        {
-            // Set specific parameters and use a default value if not present.
-            AddVec4("vec4Param", genericMaterial, material, RenderSettings.Instance.ParamId, new Vector4(0));
-
-            // Assume no edge lighting if not present.
-            AddVec4("paramA6", genericMaterial, material, 0xA6, new Vector4(0));
-
-            // Some sort of skin subsurface color?
-            AddVec4("paramA3", genericMaterial, material, 0xA3, new Vector4(1));
-
-            // Sprite sheet UV parameters.
-            AddVec4("paramAA", genericMaterial, material, 0xAA, new Vector4(1));
-
-            // Enables/disables specular occlusion.
-            AddBool("paramE9", genericMaterial, material, 0xE9, true);
-
-            // Controls anisotropic specular.
-            AddFloat("paramCA", genericMaterial, material, 0xCA, 0.0f);
-
-            // Alpha offset.
-            AddVec4("param98", genericMaterial, material, 0x98, new Vector4(1, 0, 0, 0));
-        }
-
-        private static void AddMaterialTextures(Material material, GenericMaterial genericMaterial)
-        {
-            // Use black for the default value.
-            // Some materials seem to use emission as the main diffuse.
-            AddTexture("colMap", material.col, genericMaterial);
-
-            // Use the first texture for both layers if the second layer isn't present.
-            if (material.HasCol2)
-                genericMaterial.AddTexture("col2Map", material.col2);
-            else
-                genericMaterial.AddTexture("col2Map", material.col);
-
-            AddTexture("prmMap", material.prm, genericMaterial);
-            AddTexture("norMap", material.nor, genericMaterial);
-            AddTexture("emiMap", material.emi, genericMaterial);
-            AddTexture("bakeLitMap", material.bakeLit, genericMaterial);
-            AddTexture("gaoMap", material.gao, genericMaterial);
-        }
-
-        private static void AddRenderModeTextures(GenericMaterial genericMaterial)
-        {
-            genericMaterial.AddTexture("uvPattern", defaultTextures.uvPattern);
-        }
-
-        private static void AddImageBasedLightingTextures(GenericMaterial genericMaterial)
-        {
-            genericMaterial.AddTexture("diffusePbrCube", defaultTextures.diffusePbr);
-            genericMaterial.AddTexture("specularPbrCube", defaultTextures.specularPbr);
-            genericMaterial.AddTexture("iblLut", defaultTextures.iblLut);
-        }
-
-        private static void AddTexture(string name, Texture texture, GenericMaterial genericMaterial)
-        {
-            genericMaterial.AddTexture(name, texture);
-        }
-
-        private static void AddBool(string name, GenericMaterial genericMaterial, Material source, long paramId, bool defaultValue)
-        {
-            if (source.boolByParamId.ContainsKey(paramId))
-            {
-                var value = source.boolByParamId[paramId];
-                genericMaterial.AddBoolToInt(name, value);
-            }
-            else
-            {
-                genericMaterial.AddBoolToInt(name, defaultValue);
-            }
-        }
-
-        private static void AddFloat(string name, GenericMaterial genericMaterial, Material source, long paramId, float defaultValue)
-        {
-            if (source.floatByParamId.ContainsKey(paramId))
-            {
-                var value = source.floatByParamId[paramId];
-                genericMaterial.AddFloat(name, value);
-            }
-            else
-            {
-                genericMaterial.AddFloat(name, defaultValue);
-            }
-        }
-
-        private static void AddVec4(string name, GenericMaterial genericMaterial, Material source, long paramId, Vector4 defaultValue)
-        {
-            if (source.vec4ByParamId.ContainsKey(paramId))
-            {
-                var value = source.vec4ByParamId[paramId];
-                genericMaterial.AddVector4(name, value);
-            }
-            else if (source.boolByParamId.ContainsKey(paramId))
-            {
-                var value = source.boolByParamId[paramId];
-                if (value)
-                    genericMaterial.AddVector4(name, new Vector4(1, 0, 1, 0));
-                else
-                    genericMaterial.AddVector4(name, new Vector4(0, 0, 1, 0));
-            }
-            else if (source.floatByParamId.ContainsKey(paramId))
-            {
-                var value = source.floatByParamId[paramId];
-                genericMaterial.AddVector4(name, new Vector4(value, value, value, 0));
-            }
-            else
-            {
-                genericMaterial.AddVector4(name, defaultValue);
-            }
         }
 
         public void Render(Camera Camera)
