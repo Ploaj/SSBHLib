92fba65aebe0761aec35fba83bd0444c34526d64
ploaj_the_otter <ploaj_the_otter@yahoo.com>
2019-01-11 22:23:31 -0600

    no ignore my stuff

---
+++ SSBHLib/Tools/SSBHMeshMaker.cs
@@ -0,0 +1,421 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using SSBHLib.Formats.Meshes;
+using System.Linq;
+
+namespace SSBHLib.Tools
+{
+    public enum MESHAttribute
+    {
+        Position0,
+        Normal0,
+        Tangent0,
+        map1,
+        uvSet,
+        uvSet1,
+        uvSet2,
+        colorSet1,
+        colorSet2,
+        colorSet2_1,
+        colorSet2_2,
+        colorSet2_3,
+        colorSet3,
+        colorSet4,
+        colorSet5,
+        colorSet6,
+        colorSet7,
+        bake1,
+    }
+
+    /// <summary>
+    /// Helps generate a MESH file
+    /// </summary>
+    public class SSBHMeshMaker
+    {
+        private class TempMesh
+        {
+            public string Name;
+            public string ParentBone;
+            public int VertexCount;
+            public Dictionary<MESHAttribute, float[]> VertexData = new Dictionary<MESHAttribute, float[]>();
+            public List<uint> Indices = new List<uint>();
+            public List<SSBHVertexInfluence> Influences = new List<SSBHVertexInfluence>();
+        }
+
+        private TempMesh CurrentMesh;
+        private List<TempMesh> meshes = new List<TempMesh>();
+
+        /// <summary>
+        /// Begins creating a new mesh object with given attributes
+        /// </summary>
+        /// <param name="name">The name of the Mesh Object</param>
+        /// <param name="Indices">The vertex indices as triangles</param>
+        public void StartMeshObject(string name, uint[] Indices, SSBHVertexAttribute[] Positions, string ParentBoneName = "")
+        {
+            CurrentMesh = new TempMesh();
+            CurrentMesh.Name = name;
+            CurrentMesh.ParentBone = ParentBoneName;
+            CurrentMesh.Indices.AddRange(Indices);
+            CurrentMesh.VertexCount = Positions.Length;
+            meshes.Add(CurrentMesh);
+            AddAttributeToMeshObject(MESHAttribute.Position0, Positions);
+        }
+
+        /// <summary>
+        /// Adds new attribute data to the mesh object
+        /// Note: must call StartMeshObject first
+        /// </summary>
+        /// <param name="attribute"></param>
+        /// <param name="values"></param>
+        public void AddAttributeToMeshObject(MESHAttribute attribute, SSBHVertexAttribute[] Values)
+        {
+            if (CurrentMesh == null)
+                return;
+
+            int Size = GetAttributeSize(attribute);
+            float[] values = new float[Values.Length * Size];
+            for (int i = 0; i < Values.Length; i++)
+            {
+                if(Size > 0)
+                    values[i * Size + 0] = Values[i].X;
+                if (Size > 1)
+                    values[i * Size + 1] = Values[i].Y;
+                if (Size > 2)
+                    values[i * Size + 2] = Values[i].Z;
+                if (Size > 3)
+                    values[i * Size + 3] = Values[i].W;
+            }
+
+            CurrentMesh.VertexData.Add(attribute, values);
+        }
+
+        /// <summary>
+        /// Attaches rigging information to mesh object
+        /// Note: must call StartMeshObject first
+        /// </summary>
+        public void AttachRiggingToMeshObject(SSBHVertexInfluence[] influences)
+        {
+            if (CurrentMesh == null)
+                return;
+            CurrentMesh.Influences.AddRange(influences);
+        }
+
+        /// <summary>
+        /// Creates and returns a mesh file
+        /// </summary>
+        /// <returns></returns>
+        public MESH GetMeshFile()
+        {
+            MESH mesh = new MESH();
+            //TODO: bounding box stuff
+            // Riggin
+            mesh.Objects = new MeshObject[meshes.Count];
+
+            // create mesh objects and buffers
+            BinaryWriter vertexBuffer1 = new BinaryWriter(new MemoryStream());
+            BinaryWriter vertexBuffer2 = new BinaryWriter(new MemoryStream()); // there are actually 4 buffers, but only 2 seem to be used
+            BinaryWriter indexBuffer = new BinaryWriter(new MemoryStream());
+            int finalBufferOffset = 0;
+            int meshIndex = 0;
+            Dictionary<string, int> MeshGroups = new Dictionary<string, int>();
+            List<MeshRiggingGroup> RiggingGroups = new List<MeshRiggingGroup>();
+            foreach (var tempmesh in meshes)
+            {
+                MeshObject mo = new MeshObject();
+                mo.Name = tempmesh.Name;
+                if (MeshGroups.ContainsKey(mo.Name))
+                    MeshGroups[mo.Name] += 1;
+                else
+                    MeshGroups.Add(mo.Name, 0);
+
+                mo.SubMeshIndex = MeshGroups[mo.Name];
+
+                // Create Rigging
+                RiggingGroups.Add(SSBHRiggingCompiler.CreateRiggingGroup(mo.Name, (int)mo.SubMeshIndex, tempmesh.Influences.ToArray()));
+
+                // set object
+                mesh.Objects[meshIndex++] = mo;
+
+                mo.ParentBoneName = tempmesh.ParentBone;
+                if(tempmesh.ParentBone == null || tempmesh.ParentBone.Equals(""))
+                    mo.HasRigging = 1;
+                
+                int Stride1 = 0;
+                int Stride2 = 0;
+
+                mo.VertexOffset = (int)vertexBuffer1.BaseStream.Length;
+                mo.VertexOffset2 = (int)vertexBuffer2.BaseStream.Length;
+                mo.ElementOffset = (uint)indexBuffer.BaseStream.Length;
+
+                // gather strides
+                mo.Attributes = new MeshAttribute[tempmesh.VertexData.Count];
+                int attributeIndex = 0;
+                foreach (var keypair in tempmesh.VertexData)
+                {
+                    MeshAttribute attr = new MeshAttribute();
+                    attr.Name = GetAttributeName(keypair.Key);
+                    attr.Index = GetAttributeIndex(keypair.Key);
+                    attr.BufferIndex = GetBufferIndex(keypair.Key);
+                    attr.DataType = GetAttributeDataType(keypair.Key);
+                    attr.BufferOffset = (GetBufferIndex(keypair.Key) == 0) ? Stride1 : Stride2;
+                    attr.AttributeStrings = new MeshAttributeString[] { new MeshAttributeString() { Name = keypair.Key.ToString() } };
+                    mo.Attributes[attributeIndex++] = attr;
+
+                    if (GetBufferIndex(keypair.Key) == 0)
+                        Stride1 += GetAttributeSize(keypair.Key) * GetAttributeDataSize(keypair.Key);
+                    else
+                        Stride2 += GetAttributeSize(keypair.Key) * GetAttributeDataSize(keypair.Key);
+
+                    
+                }
+
+                // now that strides are known...
+                long Buffer1Start = vertexBuffer1.BaseStream.Length;
+                long Buffer2Start = vertexBuffer2.BaseStream.Length;
+                vertexBuffer1.Write(new byte[Stride1 * tempmesh.VertexCount]);
+                vertexBuffer2.Write(new byte[Stride2 * tempmesh.VertexCount]);
+                attributeIndex = 0;
+                foreach (var keypair in tempmesh.VertexData)
+                {
+                    var attr = mo.Attributes[attributeIndex++];
+                    float[] Data = keypair.Value;
+                    var buffer = attr.BufferIndex == 0 ? vertexBuffer1 : vertexBuffer2;
+                    int bufferOffset = (int)(attr.BufferIndex == 0 ? Buffer1Start : Buffer2Start);
+                    int stride = (attr.BufferIndex == 0 ? Stride1 : Stride2);
+                    int size = GetAttributeSize(keypair.Key);
+                    for (int vertexIndex = 0; vertexIndex < tempmesh.VertexCount; vertexIndex++)
+                    {
+                        buffer.Seek(bufferOffset + stride * vertexIndex + attr.BufferOffset, SeekOrigin.Begin);
+                        for(int j = 0; j < size; j++)
+                        {
+                            WriteType(buffer, attr.DataType, Data[vertexIndex * size + j]);
+                        }
+                    }
+                    // seek to end just to make sure
+                    buffer.Seek((int)buffer.BaseStream.Length, SeekOrigin.Begin);
+                }
+
+                mo.FinalBufferOffset = finalBufferOffset;
+                finalBufferOffset += (4 + Stride1) * tempmesh.VertexCount;
+                mo.VertexCount = tempmesh.VertexCount;
+                mo.IndexCount = tempmesh.Indices.Count;
+                mo.Stride = Stride1;
+                mo.Stride2 = Stride2;
+                
+                // write index buffer
+                if(tempmesh.VertexCount > ushort.MaxValue)
+                {
+                    mo.DrawElementType = 1;
+                    foreach (var i in tempmesh.Indices)
+                        indexBuffer.Write(i);
+                }else
+                {
+                    foreach (var i in tempmesh.Indices)
+                        indexBuffer.Write((ushort)i);
+                }
+            }
+
+            mesh.PolygonIndexSize = indexBuffer.BaseStream.Length;
+            mesh.BufferSizes = new int[] { (int)vertexBuffer1.BaseStream.Length, (int)vertexBuffer2.BaseStream.Length, 0, 0};
+            mesh.PolygonBuffer = ((MemoryStream)indexBuffer.BaseStream).ToArray();
+            Console.WriteLine(mesh.PolygonBuffer.Length + " " + indexBuffer.BaseStream.Length);
+            mesh.VertexBuffers = new MeshBuffer[]
+            {
+                new MeshBuffer(){ Buffer = ((MemoryStream)vertexBuffer1.BaseStream).ToArray() },
+                new MeshBuffer(){ Buffer = ((MemoryStream)vertexBuffer2.BaseStream).ToArray() },
+                new MeshBuffer(){ Buffer = new byte[0] },
+                new MeshBuffer(){ Buffer = new byte[0] }
+            };
+
+
+            mesh.RiggingBuffers = RiggingGroups.ToArray().OrderBy(o => o.Name, StringComparer.Ordinal).ToArray();
+
+            vertexBuffer1.Close();
+            vertexBuffer2.Close();
+            indexBuffer.Close();
+
+            return mesh;
+        }
+
+        private void WriteType(BinaryWriter writer, int type, float value)
+        {
+            switch (type)
+            {
+                case 0: writer.Write(value); break;
+                case 2: writer.Write((byte)value); break;
+                case 5: writer.Write((ushort)fromFloat(value)); break;
+                case 8: writer.Write((ushort)fromFloat(value)); break;
+                default:
+                    break;
+            }
+        }
+
+        private int GetBufferIndex(MESHAttribute attribute)
+        {
+            switch (attribute)
+            {
+                case MESHAttribute.Position0:
+                case MESHAttribute.Normal0:
+                case MESHAttribute.Tangent0:
+                    return 0;
+                default:
+                    return 1;
+            }
+        }
+
+        public static int GetAttributeSize(MESHAttribute attribute)
+        {
+            switch (attribute)
+            {
+                case MESHAttribute.Position0:
+                    return 3;
+                case MESHAttribute.Normal0:
+                    return 4;
+                case MESHAttribute.Tangent0:
+                    return 4;
+                case MESHAttribute.map1:
+                case MESHAttribute.uvSet:
+                case MESHAttribute.uvSet1:
+                case MESHAttribute.uvSet2:
+                case MESHAttribute.bake1:
+                    return 2;
+                case MESHAttribute.colorSet1:
+                case MESHAttribute.colorSet2:
+                case MESHAttribute.colorSet2_1:
+                case MESHAttribute.colorSet2_2:
+                case MESHAttribute.colorSet2_3:
+                case MESHAttribute.colorSet3:
+                case MESHAttribute.colorSet4:
+                case MESHAttribute.colorSet5:
+                case MESHAttribute.colorSet6:
+                case MESHAttribute.colorSet7:
+                    return 4;
+                default:
+                    return 3;
+            }
+        }
+
+
+        private string GetAttributeName(MESHAttribute attribute)
+        {
+            switch (attribute)
+            {
+                case MESHAttribute.Tangent0:
+                    return "map1";
+                default:
+                    return attribute.ToString();
+            }
+        }
+
+        private int GetAttributeDataSize(MESHAttribute attribute)
+        {
+            switch (GetAttributeDataType(attribute))
+            {
+                case 0: return 4;
+                case 2: return 1;
+                case 5: return 2;
+                case 8: return 2;
+                default:
+                    return 2;
+            }
+        }
+
+        private int GetAttributeDataType(MESHAttribute attribute)
+        {
+            switch (attribute)
+            {
+                case MESHAttribute.Position0:
+                    return 0;
+                case MESHAttribute.Normal0:
+                case MESHAttribute.Tangent0:
+                    return 5;
+                case MESHAttribute.map1:
+                case MESHAttribute.uvSet:
+                case MESHAttribute.uvSet1:
+                case MESHAttribute.uvSet2:
+                case MESHAttribute.bake1:
+                    return 8;
+                case MESHAttribute.colorSet1:
+                case MESHAttribute.colorSet2:
+                case MESHAttribute.colorSet2_1:
+                case MESHAttribute.colorSet2_2:
+                case MESHAttribute.colorSet2_3:
+                case MESHAttribute.colorSet3:
+                case MESHAttribute.colorSet4:
+                case MESHAttribute.colorSet5:
+                case MESHAttribute.colorSet6:
+                case MESHAttribute.colorSet7:
+                    return 2;
+                default:
+                    return -1;
+            }
+        }
+
+        private int GetAttributeIndex(MESHAttribute attribute)
+        {
+            switch (attribute)
+            {
+                case MESHAttribute.Position0:
+                    return 0;
+                case MESHAttribute.Normal0:
+                    return 1;
+                case MESHAttribute.Tangent0:
+                    return 3;
+                case MESHAttribute.map1:
+                    return 4;
+                case MESHAttribute.uvSet:
+                case MESHAttribute.uvSet1:
+                case MESHAttribute.uvSet2:
+                case MESHAttribute.bake1:
+                case MESHAttribute.colorSet1:
+                    return 5;
+                case MESHAttribute.colorSet2:
+                case MESHAttribute.colorSet2_1:
+                case MESHAttribute.colorSet2_2:
+                case MESHAttribute.colorSet2_3:
+                case MESHAttribute.colorSet3:
+                case MESHAttribute.colorSet4:
+                case MESHAttribute.colorSet5:
+                case MESHAttribute.colorSet6:
+                case MESHAttribute.colorSet7:
+                default:
+                    return -1;
+            }
+        }
+
+
+        public static int SingleToInt32Bits(float value)
+        {
+            return BitConverter.ToInt32(BitConverter.GetBytes(value), 0);
+        }
+
+        public static int fromFloat(float fval)
+        {
+            int fbits = SingleToInt32Bits(fval);
+            int sign = fbits >> 16 & 0x8000;          // sign only
+            int val = (fbits & 0x7fffffff) + 0x1000; // rounded value
+
+            if (val >= 0x47800000)               // might be or become NaN/Inf
+            {                                     // avoid Inf due to rounding
+                if ((fbits & 0x7fffffff) >= 0x47800000)
+                {                                 // is or must become NaN/Inf
+                    if (val < 0x7f800000)        // was value but too large
+                        return sign | 0x7c00;     // make it +/-Inf
+                    return sign | 0x7c00 |        // remains +/-Inf or NaN
+                        (fbits & 0x007fffff) >> 13; // keep NaN (and Inf) bits
+                }
+                return sign | 0x7bff;             // unrounded not quite Inf
+            }
+            if (val >= 0x38800000)               // remains normalized value
+                return sign | val - 0x38000000 >> 13; // exp - 127 + 15
+            if (val < 0x33000000)                // too small for subnormal
+                return sign;                      // becomes +/-0
+            val = (fbits & 0x7fffffff) >> 23;  // tmp exp for subnormal calc
+            return sign | ((fbits & 0x7fffff | 0x800000) // add subnormal bit
+                + (0x800000 >> val - 102)     // round depending on cut off
+                >> 126 - val);   // div by 2^(1-(exp-127+15)) and >> 13 | exp=0
+        }
+
+    }
+}
---
+++ SSBHLib/Tools/SSBHRiggingCompiler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using SSBHLib.Formats.Meshes;
+
+namespace SSBHLib.Tools
+{
+    public class SSBHRiggingCompiler
+    {
+
+        public static MeshRiggingGroup CreateRiggingGroup(string MeshName, int MeshIndex, SSBHVertexInfluence[] influences)
+        {
+            var group = new MeshRiggingGroup();
+            group.Name = MeshName;
+            group.SubMeshIndex = MeshIndex;
+
+            Dictionary<string, List<byte>> boneNameToData = new Dictionary<string, List<byte>>();
+            Dictionary<ushort, int> vertexToWeightCount = new Dictionary<ushort, int>();
+            int MaxInfluenceCount = 0;
+
+            foreach(var influence in influences)
+            {
+                // get byte list
+                if (!boneNameToData.ContainsKey(influence.BoneName))
+                {
+                    boneNameToData.Add(influence.BoneName, new List<byte>());
+                }
+
+                var bytes = boneNameToData[influence.BoneName];
+                bytes.AddRange(BitConverter.GetBytes(influence.VertexIndex));
+                bytes.AddRange(BitConverter.GetBytes(influence.Weight));
+
+                if (!vertexToWeightCount.ContainsKey(influence.VertexIndex))
+                {
+                    vertexToWeightCount.Add(influence.VertexIndex, 0);
+                }
+                vertexToWeightCount[influence.VertexIndex]++;
+                MaxInfluenceCount = Math.Max(MaxInfluenceCount, vertexToWeightCount[influence.VertexIndex]);
+            }
+
+            // create bone groups
+            group.Flags = 0x0100 | MaxInfluenceCount;
+            group.Buffers = new MeshBoneBuffer[boneNameToData.Count];
+            int bufindex = 0;
+            foreach(var pair in boneNameToData)
+            {
+                group.Buffers[bufindex++] = new MeshBoneBuffer() { BoneName = pair.Key, Data = pair.Value.ToArray() };
+            }
+
+            return group;
+        }
+
+    }
+}
