5a77a71b7cf78cb5e4fa65186816e7239254bbb7
ploaj_the_otter <ploaj_the_otter@yahoo.com>
2019-01-18 19:46:56 -0600

    added missing AnimTrackCustomVector4

---
+++ SSBHLib/Tools/AnimTrackCustomVector4.cs
@@ -0,0 +1,26 @@
+﻿namespace SSBHLib.Tools
+{
+    /// <summary>
+    /// Represents a generic vector 4
+    /// </summary>
+    public struct AnimTrackCustomVector4
+    {
+        public float X;
+        public float Y;
+        public float Z;
+        public float W;
+
+        public AnimTrackCustomVector4(float x, float y, float z, float w)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+            W = w;
+        }
+
+        public override string ToString()
+        {
+            return $"({X}, {Y}, {Z}, {W})";
+        }
+    }
+}
