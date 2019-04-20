951ccf385ca0f350ee9ee5e1e1485a844e2528d8
ploaj_the_otter <ploaj_the_otter@yahoo.com>
2019-04-20 16:26:02 -0500

    added missing files

---
+++ SSBHLib/Tools/BoundingBoxGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSBHLib.Tools
+{
+    public class BoundingBoxGenerator
+    {
+        /// <summary>
+        /// Generates a very simple Axis Aligned Bounding Box
+        /// </summary>
+        /// <param name="points"></param>
+        public static void GenerateAABB(IEnumerable<SSBHVertexAttribute> points, out SSBHVertexAttribute max, out SSBHVertexAttribute min)
+        {
+            max = new SSBHVertexAttribute(-float.MaxValue, -float.MaxValue, -float.MaxValue);
+            min = new SSBHVertexAttribute(float.MaxValue, float.MaxValue, float.MaxValue);
+
+            foreach (SSBHVertexAttribute p in points)
+            {
+                max.X = Math.Max(max.X, p.X);
+                max.Y = Math.Max(max.Y, p.Y);
+                max.Z = Math.Max(max.Z, p.Z);
+                min.X = Math.Min(min.X, p.X);
+                min.Y = Math.Min(min.Y, p.Y);
+                min.Z = Math.Min(min.Z, p.Z);
+            }
+        }
+    }
+}
---
+++ SSBHLib/Tools/SSBHAnimTrackEncoder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using SSBHLib.Formats.Animation;
+using SSBHLib.IO;
+using System.IO;
+
+namespace SSBHLib.Tools
+{
+    /// <summary>
+    /// Encoder for ANIM track data
+    /// </summary>
+    public class SSBHAnimTrackEncoder
+    {
+
+    }
+}
