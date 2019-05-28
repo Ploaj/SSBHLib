a0ce0699e56dcfc9f55a807e31763869ad555f33
ploaj_the_otter <ploaj_the_otter@yahoo.com>
2019-05-28 12:06:12 -0500

    stop excluding my files!

---
+++ SSBHLib/Tools/AnimTrackTexture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace SSBHLib.Tools
+{
+    /// <summary>
+    /// Unknown texture information stored in animation files
+    /// </summary>
+    public class AnimTrackTexture
+    {
+        public float[] Floats;
+
+        public AnimTrackTexture(float[] floats)
+        {
+            this.Floats = floats;
+        }
+
+        public override string ToString()
+        {
+            return "{" + String.Join(",", Floats) + "}";
+        }
+    }
+}
