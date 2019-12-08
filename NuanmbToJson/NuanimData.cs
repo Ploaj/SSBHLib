fc01c15b1a00d4c7a93b2d591ab92090cc8e443e
SMG <scandanavianmountaingoat@gmail.com>
2019-12-08 16:18:39 -0600

    add project to serialize nuanim to json

---
+++ NuanmbToJson/NuanimData.cs
@@ -0,0 +1,73 @@
+﻿using SSBHLib;
+using SSBHLib.Formats.Animation;
+using SSBHLib.Tools;
+using System;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace NuanmbToJson
+{
+    class AnimTrackData
+    {
+        public string Name { get; set; }
+
+        public object[] Values { get; set; }
+
+        public AnimTrackData(AnimTrack track, SsbhAnimTrackDecoder decoder)
+        {
+            Name = track.Name;
+            Values = decoder.ReadTrack(track);
+        }
+    }
+
+    class AnimNodeData
+    {
+        public string Name { get; set; }
+
+        public AnimTrackData[] Tracks { get; set; }
+
+        public AnimNodeData(AnimNode node, SsbhAnimTrackDecoder decoder)
+        {
+            Name = node.Name;
+            Tracks = node.Tracks.Select(track => new AnimTrackData(track, decoder)).ToArray();
+        }
+    }
+
+    class AnimGroupData
+    {
+        [JsonConverter(typeof(StringEnumConverter))]
+        public AnimType Type { get; set; }
+
+        public AnimNodeData[] Nodes { get; set; }
+
+        public AnimGroupData(AnimGroup group, SsbhAnimTrackDecoder decoder)
+        {
+            Type = group.Type;
+            Nodes = group.Nodes.Select(node => new AnimNodeData(node, decoder)).ToArray();
+        }
+    }
+
+    class NuanimData
+    {
+        private readonly SsbhAnimTrackDecoder decoder;
+
+        private readonly Anim animation;
+
+        public AnimGroupData[] Animations { get; set; }
+
+        public NuanimData(string path)
+        {
+            if (Ssbh.TryParseSsbhFile(path, out SsbhFile ssbhFile))
+            {
+                animation = ssbhFile as Anim;
+                if (animation == null)
+                    throw new NullReferenceException("Could not parse file.");
+
+                decoder = new SsbhAnimTrackDecoder(animation);
+
+                Animations = animation.Animations.Select(group => new AnimGroupData(group, decoder)).ToArray();
+            }
+        }
+    }
+}
---
+++ NuanmbToJson/Program.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace NuanmbToJson
+{
+    class Program
+    {
+        private static string GetFullPathWithoutExtension(string path)
+        {
+            return Path.Combine(Path.GetDirectoryName(path), Path.GetFileNameWithoutExtension(path));
+        }
+
+        private static void SerializeNuanim(string input, string output)
+        {
+            var nuanim = new NuanimData(input);
+            var jsonText = JsonConvert.SerializeObject(nuanim, Formatting.Indented);
+            File.WriteAllText(output, jsonText);
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 1 || args.Length > 2)
+            {
+                Console.WriteLine("Usage: NuanmbToJson.exe <input> [output]");
+                return;
+            }
+
+            if (args.Length == 2)
+            {
+                SerializeNuanim(args[0], args[1]);
+            }
+            else
+            {
+                var output = $"{GetFullPathWithoutExtension(args[0])}.json";
+                SerializeNuanim(args[0], output);
+            }
+        }
+    }
+}
