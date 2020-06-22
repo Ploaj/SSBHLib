041af61072789e7ae8d9d066f8d27d3f7e79e61c
SMG <scandanavianmountaingoat@gmail.com>
2020-06-22 11:05:01 -0500

    add WIP WPF rewrite for cross mod

---
+++ CrossModGui/Views/CameraSettingsWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CrossModGui.Views
+{
+    /// <summary>
+    /// Interaction logic for CameraSettingsWindow.xaml
+    /// </summary>
+    public partial class CameraSettingsWindow : Window
+    {
+        public CameraSettingsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
