85060ea01e6e3662220013a36660e371b1c725dd
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2020-07-03 11:24:27 -0500

    set non renderable node text to grey

---
+++ CrossModGui/Converters/ColorBrushConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace CrossModGui.Converters
+{
+    [ValueConversion(typeof(System.Drawing.Color), typeof(Brush))]
+
+	public class ColorBrushConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			var color = (System.Drawing.Color)value;
+			// The default ForeColor for FileNodes has 0 alpha, so just hard code the alpha for now.
+			return new SolidColorBrush(Color.FromArgb(255, color.R, color.G, color.B));
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			// This conversion isn't needed.
+			return null;
+		}
+	}
+}
