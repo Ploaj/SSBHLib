a78716967a775d4a93ed358115a2a6cb313b835c
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2020-07-31 21:31:31 -0500

    add batch processing project

---
+++ SSBHBatchProcess/Program.cs
@@ -0,0 +1,37 @@
+﻿using SSBHLib;
+using SSBHLib.Formats.Meshes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSBHBatchProcess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage: SSBHBatchProcess.exe <SSBH source folder>");
+                return;
+            }
+            var sourceFolder = args[0];
+
+            foreach (var file in Directory.EnumerateFiles(sourceFolder, $"*.nuanmb", SearchOption.AllDirectories))
+            {
+                try
+                {
+                    Ssbh.TryParseSsbhFile(file, out SsbhFile ssbhFile);
+                }
+                catch (Exception)
+                {
+                    //Console.WriteLine(file);
+                    //Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
