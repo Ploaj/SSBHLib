96b01b9a487b26aad109efefb84aca8815bed71f
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2020-08-24 19:45:40 -0500

    add user control for float editor

---
+++ CrossModGui/UserControls/LabeledFloatEditor.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CrossModGui.UserControls
+{
+    /// <summary>
+    /// Interaction logic for LabeledFloatEditor.xaml
+    /// </summary>
+    public partial class LabeledFloatEditor : UserControl
+    {
+        public static DependencyProperty ValueProperty = DependencyProperty.Register(nameof(Value), typeof(float), typeof(LabeledFloatEditor), 
+            new FrameworkPropertyMetadata(0f, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public static DependencyProperty MinProperty = DependencyProperty.Register(nameof(Min), typeof(float), typeof(LabeledFloatEditor),
+            new FrameworkPropertyMetadata(0f));
+
+        public static DependencyProperty MaxProperty = DependencyProperty.Register(nameof(Max), typeof(float), typeof(LabeledFloatEditor),
+            new FrameworkPropertyMetadata(1f));
+
+        public static DependencyProperty LabelProperty = DependencyProperty.Register(nameof(Label), typeof(string), typeof(LabeledFloatEditor),
+            new FrameworkPropertyMetadata(""));
+
+        public static DependencyProperty LabelWidthProperty = DependencyProperty.Register(nameof(LabelWidth), typeof(double), typeof(LabeledFloatEditor),
+            new FrameworkPropertyMetadata(120.0));
+
+        public float Value 
+        {
+            get => (float)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value); 
+        }
+
+        public float Min
+        {
+            get => (float)GetValue(MinProperty);
+            set => SetValue(MinProperty, value);
+        }
+
+        public float Max
+        {
+            get => (float)GetValue(MinProperty);
+            set => SetValue(MinProperty, value);
+        }
+
+        public double LabelWidth
+        {
+            get => (double)GetValue(MinProperty);
+            set => SetValue(MinProperty, value);
+        }
+
+        public string Label
+        {
+            get => (string)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
+        }
+
+        public LabeledFloatEditor()
+        {
+            InitializeComponent();
+        }
+    }
+}
