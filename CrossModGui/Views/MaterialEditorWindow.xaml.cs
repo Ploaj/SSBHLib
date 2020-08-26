ac064d1011306ab974c095a20e4646c408ef64f1
SMG <scandanavianmountaingoat@gmail.com>
2020-08-26 09:42:00 -0500

    display material id while selecting a material

--- CrossModGui/Views/MaterialEditorWindow.xaml.cs
+++ CrossModGui/Views/MaterialEditorWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using CrossMod.Tools;
+﻿using CrossMod.Rendering;
+using CrossMod.Tools;
 using CrossModGui.ViewModels;
 using System.Diagnostics;
 using System.Windows;
@@ -11,6 +12,9 @@ namespace CrossModGui.Views
     public partial class MaterialEditorWindow : Window
     {
         private readonly MaterialEditorWindowViewModel viewModel;
+
+        private RenderSettings.RenderMode previousRenderMode;
+
         public MaterialEditorWindow(MaterialEditorWindowViewModel viewModel)
         {
             this.viewModel = viewModel;
@@ -27,6 +31,19 @@ namespace CrossModGui.Views
         private void MaterialReference_Click(object sender, RoutedEventArgs e)
         {
             Process.Start(new ProcessStartInfo("https://github.com/ScanMountGoat/Smush-Material-Research/blob/master/Material%20Parameters.md"));
+        }
+
+        private void ComboBox_DropDownOpened(object sender, System.EventArgs e)
+        {
+            // Display material ID while selecting a material.
+            previousRenderMode = RenderSettings.Instance.ShadingMode;
+            RenderSettings.Instance.ShadingMode = RenderSettings.RenderMode.MaterialID;
+        }
+
+        private void ComboBox_DropDownClosed(object sender, System.EventArgs e)
+        {
+            // Restore the original shading mode after making a selection.
+            RenderSettings.Instance.ShadingMode = previousRenderMode;
         }
     }
 }
