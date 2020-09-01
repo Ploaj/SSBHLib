9e93a26cabd43bdb0672e522a7cf53bfd1a70e64
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2020-09-01 13:03:27 -0500

    test additional types for ssbh

--- SSBHBatchProcess/Program.cs
+++ SSBHBatchProcess/Program.cs
@@ -1,6 +1,8 @@
 ﻿using SSBHLib;
 using SSBHLib.Formats.Materials;
 using SSBHLib.Formats.Meshes;
+using SSBHLib.Formats.Animation;
+using SSBHLib.Formats;
 using System;
 using System.IO;
 using System.Linq;
@@ -14,6 +16,8 @@ namespace SSBHBatchProcess
             // Test exporting to make sure it's still 1:1. 
             TestFileExport<Matl>(args[0], args[1]);
             TestFileExport<Mesh>(args[2], args[3]);
+            TestFileExport<Anim>(args[4], args[5]);
+            TestFileExport<Modl>(args[6], args[7]);
         }
 
         private static void TestFileExport<T>(string input, string output) where T : SsbhFile
