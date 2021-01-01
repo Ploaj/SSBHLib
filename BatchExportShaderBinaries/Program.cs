e00dba35c630314206333829e40e754347940009
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2021-01-01 12:05:39 -0600

    add program to dump shader binaries from nushdb

---
+++ BatchExportShaderBinaries/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using SSBHLib.Formats.Rendering;
+
+namespace BatchExportShaderBinaries
+{
+    static class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: BatchExportShaderBinaries.exe <Source Folder> <Destination Folder>");
+                return;
+            }
+
+            var sourceFolder = args[0];
+            var destinationFolder = args[1];
+
+            foreach (var file in Directory.EnumerateFiles(sourceFolder, "*.nushdb*", SearchOption.AllDirectories))
+            {
+                if (!SSBHLib.Ssbh.TryParseSsbhFile<Shdr>(File.ReadAllBytes(file), out Shdr shdr))
+                    continue;
+
+                foreach (var shader in shdr.Shaders)
+                {
+                    var outputPath = Path.Combine(destinationFolder, $"{shader.Name}.bin");
+                    File.WriteAllBytes(outputPath, shader.ShaderBinary);
+                    Console.WriteLine(outputPath);
+                }
+            }
+        }
+    }
+}
