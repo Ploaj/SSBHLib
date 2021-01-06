fdc6cdaf77072e6f5836010c3070dbebc26bcc36
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2021-01-06 12:27:15 -0600

    lazy load nuanmb files

--- CrossMod/Nodes/Formats/NuanmbNode.cs
+++ CrossMod/Nodes/Formats/NuanmbNode.cs
@@ -2,18 +2,23 @@
 using CrossMod.Rendering;
 using SSBHLib;
 using SSBHLib.Formats.Animation;
+using System;
 
 namespace CrossMod.Nodes
 {
     public class NuanmbNode : FileNode
     {
-        private readonly Anim animation;
+        private readonly Lazy<Anim> animation;
 
         public NuanmbNode(string path) : base(path, "animation", true)
         {
-            Ssbh.TryParseSsbhFile(AbsolutePath, out animation);
+            animation = new Lazy<Anim>(() =>
+            {
+                Ssbh.TryParseSsbhFile(AbsolutePath, out Anim anim);
+                return anim;
+            });
         }
 
-        public IRenderableAnimation GetRenderableAnimation() => AnimToRenderable.CreateRenderableAnimation(animation);
+        public IRenderableAnimation GetRenderableAnimation() => AnimToRenderable.CreateRenderableAnimation(animation.Value);
     }
 }
