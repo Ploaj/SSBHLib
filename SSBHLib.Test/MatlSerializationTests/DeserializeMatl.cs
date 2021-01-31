21314807e4c32c3676f5de78f611fe3da0bfb2e8
SMG <scandanavianmountaingoat@gmail.com>
2021-01-31 09:39:25 -0600

    test deserializing a matl from xml

--- SSBHLib.Test/MatlSerializationTests/DeserializeMatl.cs
+++ SSBHLib.Test/MatlSerializationTests/DeserializeMatl.cs
@@ -1,13 +1,22 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SsbhLib.MatlXml;
-using SSBHLib.Formats.Materials;
 using System.IO;
+using System.Reflection;
 
 namespace SsbhLib.Test.MatlSerializationTests
 {
     [TestClass]
     public class DeserializeMatl
     {
+        private static string ReadResourceTextFile(string resourceName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            using var stream = assembly.GetManifestResourceStream(resourceName);
+            using var reader = new StreamReader(stream);
+            return reader.ReadToEnd();
+        }
+
         [TestMethod]
         public void EmptyMatl()
         {
@@ -18,6 +27,23 @@ namespace SsbhLib.Test.MatlSerializationTests
             var matl = MatlSerialization.DeserializeMatl(reader);
 
             Assert.AreEqual(0, matl.Entries.Length);
+        }
+
+        [TestMethod]
+        public void DeserializeMarioC00()
+        {
+            var source = ReadResourceTextFile("SsbhLib.Test.MatlSerializationTests.marioc00.xml");
+
+            var reader = new StringReader(source);
+            var matl = MatlSerialization.DeserializeMatl(reader);
+
+            Assert.AreEqual(12, matl.Entries.Length);
+            var entry = matl.Entries[5];
+            Assert.AreEqual("SFX_PBS_010000001800830d_opaque", entry.ShaderLabel);
+            Assert.AreEqual("EyeRD", entry.MaterialLabel);
+
+            Assert.AreEqual(20, entry.Attributes.Length);
+            Assert.AreEqual(false, entry.Attributes[3].DataObject);
         }
     }
 }
