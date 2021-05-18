59ae38ca48910a1fea6f13584ecff19f7ba6188c
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2021-05-18 10:42:16 -0500

    hide material error description if no errors detected

---
+++ CrossMod/CrossModGui/Converters/IsEmptyVisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace CrossModGui.Converters
+{
+    public class IsEmptyVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch (value)
+            {
+                case null:
+                    return Visibility.Collapsed;
+                case ICollection list:
+                    return list.Count == 0 ? Visibility.Collapsed : Visibility.Visible;
+                default:
+                    return Visibility.Visible;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
