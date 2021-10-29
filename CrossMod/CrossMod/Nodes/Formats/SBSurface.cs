370116df097686757bb74cf59d4fce857bf99d00
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2021-10-29 13:10:29 -0500

    fix bc4/bc5 rendering

--- CrossMod/CrossMod/Nodes/Formats/SBSurface.cs
+++ CrossMod/CrossMod/Nodes/Formats/SBSurface.cs
@@ -78,11 +78,9 @@ namespace CrossMod.Nodes
                 surface.Height = reader.ReadInt32();
                 surface.Depth = reader.ReadInt32();
 
-                var Format = (NUTEX_FORMAT)reader.ReadByte();
+                var format = (NUTEX_FORMAT)reader.ReadUInt16();
+                reader.ReadUInt16();
 
-                reader.ReadByte();
-
-                ushort Padding = reader.ReadUInt16();
                 reader.ReadUInt32();
 
                 int MipCount = reader.ReadInt32();
@@ -93,19 +91,20 @@ namespace CrossMod.Nodes
                 int MajorVersion = reader.ReadInt16();
                 int MinorVersion = reader.ReadInt16();
 
-                if (pixelFormatByNuTexFormat.ContainsKey(Format))
-                    surface.PixelFormat = pixelFormatByNuTexFormat[Format];
+                if (pixelFormatByNuTexFormat.ContainsKey(format))
+                    surface.PixelFormat = pixelFormatByNuTexFormat[format];
 
-                if (internalFormatByNuTexFormat.ContainsKey(Format))
-                    surface.InternalFormat = internalFormatByNuTexFormat[Format];
+                if (internalFormatByNuTexFormat.ContainsKey(format))
+                    surface.InternalFormat = internalFormatByNuTexFormat[format];
 
-                surface.PixelType = GetPixelType(Format);
+                surface.PixelType = GetPixelType(format);
 
                 reader.BaseStream.Position = 0;
                 byte[] ImageData = reader.ReadBytes(ImageSize);
 
                 try
                 {
+                    System.Diagnostics.Debug.WriteLine(FilePath);
                     surface.Arrays = SwitchSwizzler.GetImageData(surface, ImageData, MipCount);
                 }
                 catch (System.Exception)
@@ -246,10 +245,10 @@ namespace CrossMod.Nodes
             { NUTEX_FORMAT.BC2_SRGB, InternalFormat.CompressedSrgbAlphaS3tcDxt3Ext },
             { NUTEX_FORMAT.BC3_UNORM, InternalFormat.CompressedRgbaS3tcDxt5Ext },
             { NUTEX_FORMAT.BC3_SRGB, InternalFormat.CompressedSrgbAlphaS3tcDxt5Ext },
-            { NUTEX_FORMAT.BC4_UNORM, InternalFormat.Rgba },
-            { NUTEX_FORMAT.BC4_SNORM, InternalFormat.Rgba },
-            { NUTEX_FORMAT.BC5_UNORM, InternalFormat.Rgba },
-            { NUTEX_FORMAT.BC5_SNORM, InternalFormat.Rgba },
+            { NUTEX_FORMAT.BC4_UNORM, InternalFormat.CompressedRedRgtc1 },
+            { NUTEX_FORMAT.BC4_SNORM, InternalFormat.CompressedSignedRedRgtc1 },
+            { NUTEX_FORMAT.BC5_UNORM, InternalFormat.CompressedRgRgtc2 },
+            { NUTEX_FORMAT.BC5_SNORM, InternalFormat.CompressedSignedRgRgtc2 },
             { NUTEX_FORMAT.BC6_UFLOAT, InternalFormat.CompressedRgbBptcUnsignedFloat },
             { NUTEX_FORMAT.BC7_UNORM, InternalFormat.CompressedRgbaBptcUnorm },
             { NUTEX_FORMAT.BC7_SRGB, InternalFormat.CompressedSrgbAlphaBptcUnorm }
@@ -279,23 +278,24 @@ namespace CrossMod.Nodes
 
     public enum NUTEX_FORMAT
     {
-        R8G8B8A8_UNORM = 0,
-        R8G8B8A8_SRGB = 0x05,
-        R32G32B32A32_FLOAT = 0x34,
-        B8G8R8A8_UNORM = 0x50,
-        B8G8R8A8_SRGB = 0x55,
-        BC1_UNORM = 0x80,
-        BC1_SRGB = 0x85,
-        BC2_UNORM = 0x90,
-        BC2_SRGB = 0x95,
-        BC3_UNORM = 0xa0,
-        BC3_SRGB = 0xa5,
-        BC4_UNORM = 0xb0,
-        BC4_SNORM = 0xb5,
-        BC5_UNORM = 0xc0,
-        BC5_SNORM = 0xc5,
-        BC6_UFLOAT = 0xd7,
-        BC7_UNORM = 0xe0,
-        BC7_SRGB = 0xe5
+        R8G8B8A8_UNORM = 0x0400,
+        R8G8B8A8_SRGB = 0x0405,
+        R32G32B32A32_FLOAT = 0x0434,
+        B8G8R8A8_UNORM = 0x0450,
+        B8G8R8A8_SRGB = 0x0455,
+        BC1_UNORM = 0x0480,
+        BC1_SRGB = 0x0485,
+        BC2_UNORM = 0x0490,
+        BC2_SRGB = 0x0495,
+        BC3_UNORM = 0x04a0,
+        BC3_SRGB = 0x04a5,
+        BC4_UNORM = 0x0180,
+        BC4_SNORM = 0x0185,
+        BC5_UNORM = 0x0280,
+        BC5_SNORM = 0x0285,
+        BC6_UFLOAT = 0x04d7,
+        BC6_SFLOAT = 0x04d8,
+        BC7_UNORM = 0x04e0,
+        BC7_SRGB = 0x04e5,
     }
 }
